602608c02af841cfe094f0928b5f4da9a0b148e6
Your Name <sergey_umnov@mail.ru>
2017-07-19 11:10:34 +0300

    Changed settings

--- db/db/settings.py
+++ db/db/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'es50b@z#emdsr78=)ook&v=twk1!efz@%*0@#b2p&4b5qey_m%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
